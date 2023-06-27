27b6c7e2b4f56efb18ff304db8931f19114c434f
Marbun, Ova <ova.marbun@accenture.com>
2023-06-27 13:56:51 +0700

    minor update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -29,7 +29,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`]
+            images: [`${req.protocol}://${req.get('host')}/img/tours${tour.imageCover}`]
           }
         }
       }
@@ -86,7 +86,7 @@ exports.webHookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
   res.status(200).json({
